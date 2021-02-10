a3e617f99a7596c45f4a19c6e4d7c44a69c64f03
Nisal Bandara <nisal.bandara@outlook.com>
2021-02-10 13:53:34 +0530

    Add docs folder

---
+++ lib/rapidomize/transport.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Rapidomize
+  # Base class for Transport objects2
+  class Transport
+  end
+end
---
+++ lib/rapidomize/transport/http_transport.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Rapidomize
+  module Transports
+    class HTTP
+    end
+  end
+end
